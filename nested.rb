5f249e44b0e45c22f032133a016934cbfd974e45
Austin Osorio <osorio_austin@yahoo.com>
2020-01-08 05:58:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-		 	print programmer_hash[:dennis_ritchie][:languages]
+		 	print programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
